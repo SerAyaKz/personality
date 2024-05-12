1024ead9d281855f1097853878f909f80a1fa5c5
SerAyaKz <131340184+SerAyaKz@users.noreply.github.com>
2024-05-12 18:19:07 +0500

    Update airtable.js

--- src/helper/airtable.js
+++ src/helper/airtable.js
@@ -1,7 +1,7 @@
 import Airtable from 'airtable';
 
 // api keys are confidential
-const base = new Airtable({ apiKey: 'patyKDDHNZETchcvw.c08fdc9e3309071096b16c69c0c8c824a80bd753a585d3aac71b105835933be3' }).base(
+const base = new Airtable({ apiKey: 'put api key here' }).base(
   'app0qYKja2OqJ8RnF'
 );
 
