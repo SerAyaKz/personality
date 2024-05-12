68af9ae3837cba01eeda969ba3b47b834d54da90
SerAyaKz <131340184+SerAyaKz@users.noreply.github.com>
2024-05-12 18:19:24 +0500

    Update airtable.js

--- src/helper/airtable.js
+++ src/helper/airtable.js
@@ -2,7 +2,7 @@ import Airtable from 'airtable';
 
 // api keys are confidential
 const base = new Airtable({ apiKey: 'put api key here' }).base(
-  'app0qYKja2OqJ8RnF'
+  'put base'
 );
 
 export default base;
